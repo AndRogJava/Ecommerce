780d259f6ad7f1d231779668845838af6de15584
Paolo <50113488+paolinux97@users.noreply.github.com>
2021-07-01 12:34:50 +0200

    Update registration.js
    corretta parse data, messaggio errore data gestito per date comprese tra 1/1/1900 e oggi

--- ProgettoEcommerceAccenture/WebContent/js/registration.js
+++ ProgettoEcommerceAccenture/WebContent/js/registration.js
@@ -20,7 +20,7 @@ cognome.on('keyup',function(){
 	checkCognome();});
 confPassword.on('keyup',function(){
 	checkConfirmPassword();});
-dataNascita.on('keyup',function(){
+dataNascita.on('keydown keyup mousedown mouseup',function(){
 	checkData();});
 via.on('keyup',function(){
 	checkIndirizzo();});
@@ -100,12 +100,14 @@ function checkConfirmPassword(){
 
 function upData(){
 	var toDay = new Date();
-	var toDayDate = toDay.getFullYear()+'/'+(toDay.getMonth()+1)+'/'+toDay.getDate();
+	var toDayDate = toDay.getFullYear()+'-'+(toDay.getMonth()+1)+'-'+toDay.getDate();
 	return toDayDate;
 }
 
+/* corretta parse della data */
 function checkData(){
-	if(dataNascita.val() != "" && dataNascita.val() > "1900/01/01" && dataNascita.val() < upData()){
+	var date = Date.parse(dataNascita.val());
+	if(dataNascita.val() != "" && date > Date.parse("1900-01-01") && date < Date.parse(upData())){
 		$("#errorDataNascita").html("");
 		$("#errorDataNascita").addClass("hideDiv");
 		return true;
@@ -179,7 +181,3 @@ function checkCap() {
 		return false;
 	}
 }
-
-
-
-
