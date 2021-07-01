f2999c1fca3752be3939b38cd9bbd241ad9aab47
Paolo <50113488+paolinux97@users.noreply.github.com>
2021-07-01 12:29:20 +0200

    MIGLIORIA DateValidator.java
    data nascita nel futuro (compreso oggi) non verrà più memorizzata

--- ProgettoEcommerceAccenture/src/ecommerce/registration/validator/DateValidator.java
+++ ProgettoEcommerceAccenture/src/ecommerce/registration/validator/DateValidator.java
@@ -1,6 +1,7 @@
 package ecommerce.registration.validator;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.format.DateTimeParseException;
 
 public class DateValidator {
@@ -8,7 +9,8 @@ public class DateValidator {
 		try {
 			LocalDate ld = LocalDate.parse(date);
 			LocalDate after = LocalDate.of(1900, 01, 01);
-			LocalDate before = LocalDate.of(2022, 01, 01);
+			//data di oggi (zona CET)
+			LocalDate before = LocalDate.now(ZoneId.of("CET"));
 			if(ld.isBefore(before) && ld.isAfter(after))
 				return true;
 			else
